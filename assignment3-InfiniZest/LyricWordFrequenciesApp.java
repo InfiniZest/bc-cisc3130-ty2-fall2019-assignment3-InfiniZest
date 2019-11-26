908d3cd43ac8f90ec7917f7151866554e613d2ea
Imran Khan <imran.khan2373@gmail.com>
2019-11-25 23:05:28 -0500

    (CommitVer-Feature)-Added HashTable Storage

--- assignment3-InfiniZest/LyricWordFrequenciesApp.java
+++ assignment3-InfiniZest/LyricWordFrequenciesApp.java
@@ -1,25 +1,33 @@
 import java.io.File;
 import java.util.Scanner;
+import java.io.IOException;
 import java.util.ListIterator;
-import java.util.LinkedList;
+import java.util.Hashtable;
 
 public class LyricWordFrequenciesApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 	// Words from lyrics will be stored into a LinkedList
 	//LinkedList<WordFreq> wordList = new OrderedList();
 	// Reads all songs from folder
 	System.out.println("Select one of the following tracks from the list below:");
 	File folder = new File("Songs");
-	File[] songList = folder.listFiles();
+	File[] filesList = folder.listFiles();
+	Hashtable<Song, Integer> songList = new Hashtable();
 	// User selects which song to find its word frequencies
-	int index = 1;
-	for(File file : songList) {
+	Scanner getSong = new Scanner(System.in);
+	int songIndex = 1;
+	int fileIndex = 0;
+	for(File file : filesList) {
 	    if(file.isFile() & file.getName().endsWith(".txt")) {
-		System.out.println("\t" + index + ". " + file.getName());
-		index++;
+		System.out.println("\t" + songIndex + ". " + file.getName());
+		songList.put(new Song(file), songIndex);
+		songIndex++;
 	    }
-		    
+	    fileIndex++;
 	}
-	// Scanner scan = new Scanner(songFile);
+	
+	System.out.print("\t\t\tTrack Number: ");
+	// Scanner scanLyricData = new Scanner(songList[getSong.nextInt()]);
+	
     }
 }
--- assignment3-InfiniZest/Song.java
+++ assignment3-InfiniZest/Song.java
@@ -1,11 +1,18 @@
 /* This class will retrieve song lyrics */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
 
-import java.util.Hashtable<K,V>;
-
-public class Song() {
-    // Gonna use a HashTable
-    Hashtable<String, Integer> wordFrequency = new Hashtable();
-    // Constructor sets values to HashTable need a ListIterator
-    public Song() {
+public class Song {
+    private String word;
+    
+    public Song(File songName) throws FileNotFoundException {
+	Scanner songLyricsIn = new Scanner(new FileReader(songName));
+	// Belwo reads through the file until EOF
+	while(songLyricsIn.hasNext()) {
+	    word = songLyricsIn.next();
+	    System.out.println(word);
+	}
     }
 }
